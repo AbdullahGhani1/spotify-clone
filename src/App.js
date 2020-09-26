f13aa2ed275558da90e31e8716a73062af4aa84a
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2020-09-27 00:42:21 +0500

    pull-the-token-from-url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Spotify clone</h1>
+    <div className='app'>
+      <Login />
     </div>
   );
 }
---
+++ src/soptify.js
@@ -0,0 +1,30 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start
+// click login button
+// Redirect to spotify login page
+// Redirect to home page once Authorized
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const redirectUri = 'http://localhost:3000/';
+const clientId = '8765545b25344f0095aa282cbadd8093';
+
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+];
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      // #accessToken=mysecretkey&name=abdullah
+      let parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri${redirectUri}&scope=${scopes.join(
+  '%20'
+)}&response_type=token&show_dialog=true`;
